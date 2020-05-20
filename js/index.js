0745abd3d69f6bfc41fee45f18d2654ea4ef1ff0
mosesmhlwana99 <mosesmhlwana@gmail.com>
2020-05-20 11:40:13 +0100

    back

--- js/index.js
+++ js/index.js
@@ -298,47 +298,6 @@ function updatefirebase(){
 
 ///////////////////////////////comments//////////////////////////////////
 
-   //Rootref is the whole database.
-   const rootRef = firebase.database().ref();
-   //commentsRef is just the pageCountsNode
-   const commentsRef = rootRef.child('comments');
-   //Listen for click on Submit Comment button, and post comment.
-
-$("#addCom").click(function()
-{
-  console.log('button pressssssssed')
- //Replace line breaks in comment with br tags.
- var newcomment = document.getElementById('txComment').value.replace(/\n/g, "<br>");
- //Define a new, keyed post.
- var newPostRef = commentsRef.push();
- //Fill tne new keyed post with data
- newPostRef.set({
-     name: document.getElementById('tbName').value.trim(),
-     comment: newcomment.trim(),
-     frompage: location.pathname,
-     when: firebase.database.ServerValue.TIMESTAMP
- });  
-
-});
-
-function showpastcomments() {
-  console.log('button NOT pressed yet')
-  var showat = document.getElementById('pastcomments');
-  //Get comments whose from page equals this page's pathname.
-  var commentsRef = firebase.database().ref('comments/').orderByChild('frompage').equalTo(location.pathname);
-  commentsRef.once('value', function (snapshot) {
-      snapshot.forEach(function (itemSnapshot) {
-          //Get the object for one snapshot
-          var itemData = itemSnapshot.val();
-          var comment = itemData.comment;
-          var name = itemData.name;
-          var when = new Date(itemData.when).toLocaleDateString("en-us");
-          showat.innerHTML += "<li>" + comment + "<span> -- " + name + " (" + when +
-              ")</span></li>";
-      })
-  })
-}
-//Called when page first opens and also after Submit button click to show all comments for this page.
-showpastcomments()
+ 
 
 ///////////////////////////////comments//////////////////////////////////
